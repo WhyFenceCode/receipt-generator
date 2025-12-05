0912755e8b4592f935383b2a04a91a502501c785
WhyFenceCode <whyfencecode@gmail.com>
2025-12-04 20:14:11 -0800

    added sloc for github interaction

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 //File to tie together
+
+//SLOC TEST FOR NOW
+import {countTotalLines, countLinesByLanguage} from './modules/plugins/sloc.js';
+
+
+async function test() {
+  const repo = "https://github.com/facebook/react";
+
+  console.log("Counting by language...");
+  const langStats = await countLinesByLanguage(repo);
+  console.log(langStats);
+
+  console.log("Counting total lines...");
+  const total = await countTotalLines(repo);
+  console.log("Total lines:", total);
+}
+
+test();
---
+++ modules/plugins/sloc.js
@@ -0,0 +1,64 @@
+import { execFile } from "child_process";
+import { promisify } from "util";
+import simpleGit from "simple-git";
+import tmp from "tmp";
+import fs from "fs";
+import path from "path";
+
+const execFileAsync = promisify(execFile);
+
+async function runCloc(dir) {
+  const { stdout } = await execFileAsync("npx", ["cloc", dir, "--json"], {
+    maxBuffer: 1024 * 1024 * 10
+  });
+  return JSON.parse(stdout);
+}
+
+async function cloneRepo(repoUrl) {
+  const tmpDir = tmp.dirSync({ unsafeCleanup: true });
+  const git = simpleGit();
+  await git.clone(repoUrl, tmpDir.name);
+  return tmpDir;
+}
+
+export async function countLinesByLanguage(repoUrl) {
+  const tmpDir = await cloneRepo(repoUrl);
+  const result = await runCloc(tmpDir.name);
+
+  tmpDir.removeCallback();
+
+  return result;
+}
+
+function countLinesInFile(filePath) {
+  const content = fs.readFileSync(filePath, "utf8");
+  return content.split("\n").length;
+}
+
+function countLinesRecursively(dir) {
+  let total = 0;
+
+  const entries = fs.readdirSync(dir, { withFileTypes: true });
+
+  for (const entry of entries) {
+    const fullPath = path.join(dir, entry.name);
+
+    if (entry.name === ".git") continue;
+
+    if (entry.isDirectory()) {
+      total += countLinesRecursively(fullPath);
+    } else if (entry.isFile()) {
+      total += countLinesInFile(fullPath);
+    }
+  }
+  return total;
+}
+
+export async function countTotalLines(repoUrl) {
+  const tmpDir = await cloneRepo(repoUrl);
+
+  const total = countLinesRecursively(tmpDir.name);
+
+  tmpDir.removeCallback();
+  return total;
+}
